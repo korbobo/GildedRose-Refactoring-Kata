4d3bf8de331b5b55d990b7b0880b69c839213feb
mmv <email.mikhaylov@gmail.com>
2022-11-20 22:16:38 +0100

    code cleanup

--- csharpcore/GildedRoseTests/TexttestFixture.cs
+++ csharpcore/GildedRoseTests/TexttestFixture.cs
@@ -1,64 +1,87 @@
-﻿
-using GildedRoseKata;
-
-using System;
+﻿using System;
 using System.Collections.Generic;
+using GildedRose;
 
-namespace GildedRoseTests
+namespace GildedRoseTests;
+
+public static class TexttestFixture
 {
-    public static class TexttestFixture
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        Console.WriteLine("OMGHAI!");
+
+        IList<Item> Items = new List<Item>
         {
-            Console.WriteLine("OMGHAI!");
-
-            IList<Item> Items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 15,
-                    Quality = 20
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 10,
-                    Quality = 49
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 5,
-                    Quality = 49
-                },
-                // this conjured item does not work properly yet
-                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-            };
-
-            var app = new GildedRose(Items);
-
-            int days = 2;
-            if (args.Length > 0)
+            new()
             {
-                days = int.Parse(args[0]) + 1;
-            }
-
-            for (var i = 0; i < days; i++)
+                Name = "+5 Dexterity Vest",
+                SellIn = 10,
+                Quality = 20
+            },
+            new()
             {
-                Console.WriteLine("-------- day " + i + " --------");
-                Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
-                {
-                    System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
-                }
-                Console.WriteLine("");
-                app.UpdateQuality();
+                Name = "Aged Brie",
+                SellIn = 2,
+                Quality = 0
+            },
+            new()
+            {
+                Name = "Elixir of the Mongoose",
+                SellIn = 5,
+                Quality = 7
+            },
+            new()
+            {
+                Name = "Sulfuras, Hand of Ragnaros",
+                SellIn = 0,
+                Quality = 80
+            },
+            new()
+            {
+                Name = "Sulfuras, Hand of Ragnaros",
+                SellIn = -1,
+                Quality = 80
+            },
+            new()
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                SellIn = 15,
+                Quality = 20
+            },
+            new()
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                SellIn = 10,
+                Quality = 49
+            },
+            new()
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                SellIn = 5,
+                Quality = 49
+            },
+            // this conjured item does not work properly yet
+            new()
+            {
+                Name = "Conjured Mana Cake",
+                SellIn = 3,
+                Quality = 6
             }
+        };
+
+        var app = new GildedRose.GildedRose(Items);
+
+        var days = 2;
+        if (args.Length > 0) days = int.Parse(args[0]) + 1;
+
+        for (var i = 0; i < days; i++)
+        {
+            Console.WriteLine("-------- day " + i + " --------");
+            Console.WriteLine("name, sellIn, quality");
+            for (var j = 0; j < Items.Count; j++)
+                Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
+            Console.WriteLine("");
+            app.UpdateQuality();
         }
     }
 }
