3e1fd37168361b0ae2a16d30f75b2716dd5b39ab
mmv <email.mikhaylov@gmail.com>
2022-11-20 22:21:49 +0100

    introducing common function + constants

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -4,6 +4,9 @@ namespace GildedRose;
 
 public class GildedRose
 {
+    private const string AGED_BRIE = "Aged Brie";
+    private const string BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    private const string SULFURAS = "Sulfuras, Hand of Ragnaros";
     public GildedRose(IList<Item> Items) { this.Items = Items; }
     public IList<Item> Items { get; }
 
@@ -11,51 +14,58 @@ public class GildedRose
     {
         for (var i = 0; i < Items.Count; i++)
         {
-            if (Items[i].Name != "Aged Brie" &&
-                Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            var item = Items[i];
+            if (item.Name != AGED_BRIE &&
+                item.Name != BACKSTAGE)
             {
-                if (Items[i].Quality > 0)
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        Items[i].Quality = Items[i].Quality - 1;
+                if (item.Quality > 0)
+                    if (item.Name != SULFURAS)
+                        item.Quality = item.Quality - 1;
             }
             else
             {
-                if (Items[i].Quality < 50)
+                if (item.Quality < 50)
                 {
-                    Items[i].Quality = Items[i].Quality + 1;
+                    item.Quality = item.Quality + 1;
 
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Name == BACKSTAGE)
                     {
-                        if (Items[i].SellIn < 11)
-                            if (Items[i].Quality < 50)
-                                Items[i].Quality = Items[i].Quality + 1;
+                        if (item.SellIn < 11)
+                            if (item.Quality < 50)
+                                item.Quality = item.Quality + 1;
 
-                        if (Items[i].SellIn < 6)
-                            if (Items[i].Quality < 50)
-                                Items[i].Quality = Items[i].Quality + 1;
+                        if (item.SellIn < 6)
+                            if (item.Quality < 50)
+                                item.Quality = item.Quality + 1;
                     }
                 }
             }
 
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros") Items[i].SellIn = Items[i].SellIn - 1;
+            if (item.Name != SULFURAS) item.SellIn = item.SellIn - 1;
 
-            if (Items[i].SellIn < 0)
+            if (item.SellIn < 0)
             {
-                if (Items[i].Name != "Aged Brie")
+                if (item.Name != AGED_BRIE)
                 {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Name != BACKSTAGE)
                     {
-                        if (Items[i].Quality > 0)
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                Items[i].Quality = Items[i].Quality - 1;
+                        if (item.Quality > 0)
+                            if (item.Name != SULFURAS)
+                                item.Quality = item.Quality - 1;
                     }
-                    else { Items[i].Quality = Items[i].Quality - Items[i].Quality; }
+                    else { item.Quality = item.Quality - item.Quality; }
                 }
                 else
                 {
-                    if (Items[i].Quality < 50) Items[i].Quality = Items[i].Quality + 1;
+                    if (item.Quality < 50) item.Quality = item.Quality + 1;
                 }
             }
         }
+    }
+
+    private void UpdateItem(Item item, int quantityAdjustment, int sellInAdjustment)
+    {
+        item.Quality += quantityAdjustment;
+        item.SellIn += sellInAdjustment;
     }
 }
